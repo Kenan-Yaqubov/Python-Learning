7642bdc0ac53dd123c0f308fa1c35e6e8bf47f1e
YaqubovKenan <yagubov.k.c@gmail.com>
2024-08-31 00:12:50 +0400

    new lessons learned

---
+++ lesson2.py
@@ -0,0 +1,46 @@
+# BEGIN
+
+family = 'Targaryen'
+pet = 'Dragon'
+
+print(family)
+print(' and ')
+print(pet)
+
+# =======================================================
+
+euros_count = 100
+
+euro = 1.25
+dollar = 60
+
+euro_in_dollars = euros_count * euro
+print(euro_in_dollars)
+
+dollar_in_rubles = euro_in_dollars * dollar
+print(dollar_in_rubles)
+
+# ========================================================
+
+stark = 'Arya'
+
+text = f'''Do you want to eat, {stark}?
+Yes, I'm hungry, mom.'''
+
+print(text)
+
+# ========================================================
+
+one = 'Naharis'
+two = 'Mormont'
+three = 'Sand'
+
+print(f'{one[2]}{two[1]}{three[3]}{two[4]}{two[2]}')
+
+# ========================================================
+
+value = 'Hexlet'
+
+print(f'{value[2:5]}')
+
+# END
---
+++ lesson3.py
@@ -0,0 +1,31 @@
+# BEGIN
+
+# str - string :3
+# int - number :3
+# float - number with point? :3
+# abs - makes negative number regular
+
+print(str(2) + " times")
+print(int("23"))
+print(0.1 + 0.2)
+print(float(5))
+print(abs(int("-42")))
+print(abs(10 + -13))
+
+# ========================================================
+
+# len - founds the length of word
+# pow - vozvodit v stepen :3
+# round - round's the number 
+
+company1 = "Apple"
+company2 = "Samsung"
+
+company1_length = len(company1)
+company2_length = len(company2)
+
+print(company1_length + company2_length)
+print(pow(company1_length, company2_length))
+
+print(round(15.52, 1))
+# END
