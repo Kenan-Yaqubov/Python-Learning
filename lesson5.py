5cdff2bb5195e44049fb31ba442afb4865bd8f02
YaqubovKenan <yagubov.k.c@gmail.com>
2024-09-01 23:25:40 +0400

    cyclec learned

--- lesson5.py
+++ lesson5.py
@@ -35,8 +35,8 @@ def is_leap_year(year: int) -> int:
     return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
 
 
-print(is_leap_year(2016)) 
-print(is_leap_year(2019)) 
+print(is_leap_year(2016))
+print(is_leap_year(2019))
 
 
 # Число 0 эквивалентно False
@@ -53,4 +53,45 @@ def string_or_not(text):
 print(string_or_not('5'))
 print(string_or_not(5))
 
+
+# =========================================================
+
+def normalize_url(url):
+    https = 'https://'
+    if url[:8] == https:
+        return url
+    elif url[:7] == 'http://':
+        return url.replace('http://', https, 1)
+    else:
+        return https + url
+
+
+print(normalize_url("http://kenan"))
+
+
+def get_number_explanation(num: int) -> int:
+    match num:
+        case 666:
+            return 'devil number'
+        case 42:
+            return 'answer for everything'
+        case 7:
+            return 'prime number'
+        case _:
+            return "just a number"
+
+
+print(get_number_explanation(42))
+
+# =========================================================
+
+
+def print_numbers(num: int):
+    while num > 0:
+        print(num)
+        num -= 1
+    print('finished!')
+
+
+print_numbers(5)
 # END
